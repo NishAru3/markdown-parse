efe8a4e6ba3346ec16c005e444f43bfcb8cf1002
Nishanth Arumugam <nishantharu3@gmail.com>
2022-02-02 18:45:51 -0800

    Update tester

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -7,10 +7,6 @@ import java.nio.file.Path;
 import java.util.List;
 
 public class MarkdownParseTest {
-    @Test
-    public void addition() {
-        assertEquals(99999999, 1 + 1);
-    }
 
     @Test
     public void testFile1() throws IOException {
