a73427289356b0cf5600447dc9c228579704ab6b
Nishanth Arumugam <nishantharu3@gmail.com>
2022-02-25 11:26:30 -0800

    Update MarkdownParseTest.java

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -8,21 +8,21 @@ public class MarkdownParseTest {
 
     @Test
     public void testSnippet1() throws IOException{
-        String contents= Files.readString(Path.of("snippet1.md"));
+        String contents= Files.readString(Path.of("test-snippet1.md"));
         List<String> links = List.of("`google.com","google.com","ucsd.edu");
         assertEquals(links, MarkdownParse.getLinks(contents));
     }
 
     @Test
     public void testSnippet2() throws IOException{
-        String contents= Files.readString(Path.of("snippet2.md"));
+        String contents= Files.readString(Path.of("test-snippet2.md"));
         List<String> links = List.of("a.com","a.com(())","example.com");
         assertEquals(links, MarkdownParse.getLinks(contents));
     }
 
     @Test
     public void testSnippet3() throws IOException{
-        String contents= Files.readString(Path.of("snippet3.md"));
+        String contents= Files.readString(Path.of("test-snippet3.md"));
         List<String> links = List.of("https://ucsd-cse15l-w22.github.io/");
         assertEquals(links, MarkdownParse.getLinks(contents));
     }
